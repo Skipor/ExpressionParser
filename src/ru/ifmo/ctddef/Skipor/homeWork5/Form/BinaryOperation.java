0c315206757c2740c90ce93210378d519adf5d88
Vladimir Skipor <vladimirskipor@gmail.com>
2015-03-24 16:57:46 +0300

    final added

--- src/ru/ifmo/ctddef/Skipor/homeWork5/Form/BinaryOperation.java
+++ src/ru/ifmo/ctddef/Skipor/homeWork5/Form/BinaryOperation.java
@@ -5,7 +5,6 @@ import ru.ifmo.ctddef.Skipor.homeWork5.VariablesValues;
 public class BinaryOperation implements Form {
     private final Form leftArgument;
     private final Form rightArgument;
-    private final boolean isLeftA
     private final Operation operation;
 
     public BinaryOperation(Form leftArgument, Form rightArgument, Operation operation) {
